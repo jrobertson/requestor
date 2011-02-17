579f63de88d554364329ea70e04fedf0f2d5c50d
James Robertson <james@r0bertson.co.uk>
2011-02-17 21:13:46 +0000

    a dynarex file list can now be read from the url

--- lib/requestor.rb
+++ lib/requestor.rb
@@ -3,6 +3,8 @@
 # file: requestor.rb
 
 require 'open-uri'
+require 'net/http'
+require 'dynarex'
 
 class Requestor
 
@@ -10,17 +12,47 @@ class Requestor
 
     attr_reader :code
 
-    def initialize(url)
+    def initialize(url) 
       @url = url.sub(/[^\/]$/,'\0/') 
+      @file_list = false
+
+      def url.file_list?()
+        pattern = "<type>ruby_files</type>"
+
+        @file_list = false
+      
+        url = URI.parse(self)
+        code = Net::HTTP.start(url.host, url.port) do |http| 
+          http.head(url.request_uri).code 
+        end
+
+        if code == '200' then
+          buffer = open(url, 'UserAgent' => 'Ruby-Requestor').read
+          @file_list = true if buffer[/#{pattern}/i]
+        end
+    
+      end
+
       @names = []
       @code = []
+      
+      if url.file_list? then
+        @file_list = true
+        @dynarex = Dynarex.new(url) 
+      end
     end
 
     def require(file)
-      file += '.rb' unless file[/\.rb$/]
+
       unless @names.include? file then
+        if @file_list then
+          url = @dynarex.records[file][:body][:url]
+        else
+          file += '.rb' unless file[/\.rb$/]
+          url = @url + file
+        end
         @names << file 
-        @code << open(@url + file, 'UserAgent' => 'Ruby-Requestor v0.1').read
+        @code << open(url, 'UserAgent' => 'Ruby-Requestor v0.1').read
       end
     end
     
--- requestor.gemspec
+++ requestor.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name = 'requestor'
-  s.version = '0.1.2'
+  s.version = '0.1.3'
   s.summary = 'requestor'
   s.files = Dir['lib/**/*.rb']
 end
